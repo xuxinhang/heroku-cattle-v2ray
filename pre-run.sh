e52d8c94061d45b5aef9d45815c9b7aabac7a4d3
super-cattle <xuxinhang4567#126.com>
2019-02-18 16:09:16 +0800

    添加 npm run build，其中执行 chmod

---
+++ pre-run.sh
@@ -0,0 +1,8 @@
+echo ">> Pre-run start"
+
+chmod +x ./run.sh
+
+chmod +x ./v2ray
+chmod +x ./v2ctl
+
+echo ">> Pre-run end"
--- run.sh
+++ run.sh
@@ -1,4 +1,8 @@
+echo ">> Run start"
+
 echo $V2RAY_CONFIG_JSON > config.json
 chmod +x ./v2ray
 chmod +x ./v2ctl
 ./v2ray
+
+echo ">> Pre-run end"
